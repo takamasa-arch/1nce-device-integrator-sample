6e7455373317746ebc6b57ac0451d0b8f12ab2c7
tkms.hrtk@gmail.com <43411481+tkmmobile@users.noreply.github.com>
2023-01-16 00:20:55 +0900

    test

--- sample_grovepi.js
+++ sample_grovepi.js
@@ -1,7 +1,6 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 let message = ''
-let raw_message = ''
 
 var {PythonShell} = require('python-shell');
 
@@ -9,8 +8,7 @@ PythonShell.run('./lib/get_dht11.py', null, function (err, result) {
     if (err) throw err;
  
     console.log(result);
-    raw_message = 'temp:' + result[1] + ', humi:' + result[2];
-    message = Buffer.from(raw_message);
+    message = Buffer.from('temp:' + result[1] + ', humi:' + result[2]);
 
     client.send(message, process.env.UDP_PORT_TEST || 4445, '127.0.0.1', (err) => {
         if (err) console.err(err);
