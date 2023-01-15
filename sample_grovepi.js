b1ae40e25b6132d22154b3bea80207d21ea648a7
tkms.hrtk@gmail.com <43411481+tkmmobile@users.noreply.github.com>
2023-01-16 00:29:05 +0900

    test

--- sample_grovepi.js
+++ sample_grovepi.js
@@ -8,6 +8,7 @@ function getTempHumi_value() {
     PythonShell.run('./lib/get_dht11.py', null, function (err, result) {
         if (err) throw err;
         
+        console.log(result);
         message = Buffer.from('temp:' + result[1] + ', humi:' + result[2]);
 
         client.send(message, process.env.UDP_PORT_TEST || 4445, '127.0.0.1', (err) => {
