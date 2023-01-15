899fef119635e5b4937799ccdacf95fc88c620bb
tkms.hrtk@gmail.com <43411481+tkmmobile@users.noreply.github.com>
2023-01-15 23:58:12 +0900

    test

--- sample_grovepi.js
+++ sample_grovepi.js
@@ -3,5 +3,5 @@ var {PythonShell} = require('python-shell');
 PythonShell.run('./lib/get_dht11.py', null, function (err, result) {
     if (err) throw err;
  
-    console.log(result);
+    console.log(result[1]);
 });
